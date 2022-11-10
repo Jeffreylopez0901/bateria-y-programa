ec29e7f63e44b68cb2e763b6ac3bc1bd980b556f
MAYCOL JUAREZ <114685672+maycoljuarez@users.noreply.github.com>
2022-11-10 07:59:03 -0600

    Update ejercicio_5.cpp

--- ejercicio_5.cpp
+++ ejercicio_5.cpp
@@ -1,8 +1,4 @@
-/* nombre: ejercicio_5
-autor: maycol andres juarez
-lugar : ITV
-instrucciones:  raizcuadrada y  potemcia de un numero
-*/ 
+
 #include<iostream>
 #include<cmath>
 using namespace std;
